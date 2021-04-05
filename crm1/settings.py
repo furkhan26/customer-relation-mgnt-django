c57267226aa68221743fb081cfc47312043afa32
furkhan26 <furkhanmehdi26@gmail.com>
2021-04-06 02:15:28 +0530

    dj static added

--- crm1/settings.py
+++ crm1/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
